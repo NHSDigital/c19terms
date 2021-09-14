d6344b700976696475fe1b3f573d24a90fbbb229
matt.mercer <matt.mercer@nhs.net>
2021-09-14 20:04:01 +0100

    fix version import

--- pip/setup.py
+++ pip/setup.py
@@ -1,9 +1,14 @@
-import os
 from setuptools import setup
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
 
 setup(
     name='covid_19_terms',
-    version=f'{os.environ["RELEASE_VERSION"]}',
+    version=metadata.version('pip'),
     packages=['covid_19_terms'],
     long_description="UK JSON COVID SNOMED CT codes from \"COVID-19 Vaccination Codes\" https://hscic.kahootz.com/connect.ti/t_c_home/view?objectId=16878800",
     long_description_content_type='text/markdown',
@@ -12,5 +17,8 @@ setup(
     },
     package_data={
         'covid_19_terms': ['terms.json']
-    }
+    },
+    install_requires=[
+        'importlib-metadata >= 1.0 ; python_version < "3.8"',
+    ],
 )
