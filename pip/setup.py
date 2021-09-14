a6d386e2502fc021f8488ce9dc091f69c35cc4dd
matt.mercer <matt.mercer@nhs.net>
2021-09-14 19:46:04 +0100

    mm: update package descriptions

--- pip/setup.py
+++ pip/setup.py
@@ -5,6 +5,8 @@ setup(
     name='covid_19_terms',
     version=f'{os.environ["RELEASE_VERSION"]}',
     packages=['covid_19_terms'],
+    long_description="UK JSON COVID SNOMED CT codes from \"COVID-19 Vaccination Codes\" https://hscic.kahootz.com/connect.ti/t_c_home/view?objectId=16878800",
+    long_description_content_type='text/markdown',
     package_dir={
         'covid_19_terms': 'covid_19_terms'
     },
